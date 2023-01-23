7ba6e96ffc35ec92a9f66b2c3f89158009517d47
manhph2211 <maxph22112000@gmail.com>
2023-01-23 20:32:34 +0700

    add app

---
+++ app/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import re
+from pathlib import Path
+
+__version__ = "0.1.0"
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+with open(f"{BASE_DIR}/weights/model-{__version__}.pkl", "rb") as f:
+    model = pickle.load(f)
+
+def predict(text):
+    text = re.sub(r'[!@#$(),\n"%^*?\:;~`a-z]', " ", text)
+    text = re.sub(r"[[]]", " ", text)
+    pred = model.predict([text])
+    return pred
+
+
+app = FastAPI()
+
+
+class TextIn(BaseModel):
+    text: str
+
+
+class PredictionOut(BaseModel):
+    language: str
+
+
+@app.get("/")
+def home():
+    return {"health_check": "OK", "model_version": __version__ }
+
+
+@app.post("/gen", response_model=PredictionOut)
+def predict(payload: TextIn):
+    language = predict(payload.text)
+    return {"language": language}
