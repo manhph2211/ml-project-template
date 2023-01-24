f3a3379172f1a71569ca65ffab4ed6d71df697f2
manhph2211 <maxph22112000@gmail.com>
2023-01-24 22:44:59 +0700

    add

--- backend/main.py
+++ backend/main.py
@@ -15,8 +15,8 @@ __version__ = "0.1.0"
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
-with open(f"{BASE_DIR}/weights/model-{__version__}.pkl", "rb") as f:
-    model = pickle.load(f)
+# with open(f"{BASE_DIR}/weights/model-{__version__}.pkl", "rb") as f:
+#     model = pickle.load(f)
 
 def predict(text):
     pred = model.predict([text])
